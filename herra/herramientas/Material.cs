3d57468538830d882309ad0a39dad4c46dd6876d
saTmo2 <santiago.marin415@pascualbravo.edu.co>
2024-10-31 23:16:30 -0500

    Add files via upload
    inicio del proyecto

---
+++ herra/herramientas/Material.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herramientas
+{
+    using System;
+
+    public class Material
+    {
+        public string Identificador;
+        public string Titulo;
+        public DateTime FechaRegistro;
+        public int CantidadRegistrada;
+
+        public Material(string identificador, string titulo, DateTime fechaRegistro, int cantidadRegistrada)
+        {
+            Identificador = identificador;
+            Titulo = titulo;
+            FechaRegistro = fechaRegistro;
+            CantidadRegistrada = cantidadRegistrada;
+        }
+    }
+
+}
